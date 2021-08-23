3381ec10bd6776617ce8d88b9d14d80c0532e63d
LaserRock46 <bialyrobert11@gmail.com>
2021-08-23 21:01:11 +0200

    ukończony silhouette controller

--- WT X Ray/Assets/Scripts/SilhouetteController.cs
+++ WT X Ray/Assets/Scripts/SilhouetteController.cs
@@ -9,9 +9,7 @@ namespace Project.Uncategorized
     {
         #region Temp
         [Header("Temporary Things", order = 0)]
-        public Vector3 screenPosition;
-        public Vector3 silhouetteViewPosition;
-        public int[] testSorted = new int[4];
+
         #endregion
 
         #region Fields
@@ -25,11 +23,26 @@ namespace Project.Uncategorized
         public float radiusSpeed;
         [SerializeField] private Transform _silhouetteWorldCenter;
         [SerializeField] private MeshFilter _silhouetteQuad;
+        [SerializeField] private Vector3[] _frustumCornersQuad = new Vector3[4];
+
 
         #endregion
 
         #region Functions
+        Vector3 WorldToQuad(Vector3 worldSpacePosition)
+        {
+            Vector3 localPosition = _camera.transform.InverseTransformPoint(worldSpacePosition);
+            Vector3[] frustumCornersWorld = new Vector3[4];
+            _camera.CalculateFrustumCorners(new Rect(0, 0, 1, 1), localPosition.z, Camera.MonoOrStereoscopicEye.Mono, frustumCornersWorld);
 
+            float verticalLinear = Mathf.InverseLerp(frustumCornersWorld[0].y, frustumCornersWorld[1].y,localPosition.y);
+            float verticalInQuad = Mathf.Lerp(_frustumCornersQuad[0].y, _frustumCornersQuad[1].y,verticalLinear);
+            float horizontalLinear = Mathf.InverseLerp(frustumCornersWorld[0].x, frustumCornersWorld[2].x, localPosition.x);
+            float horizontalInQuad = Mathf.Lerp(_frustumCornersQuad[0].x, _frustumCornersQuad[2].x, horizontalLinear);
+            Vector3 positionInQuad = new Vector3(horizontalInQuad,verticalInQuad,0);
+
+            return positionInQuad;
+        }
         #endregion
 
 
@@ -42,18 +55,16 @@ namespace Project.Uncategorized
             FitQuadInNearPlane();
         }
         void FitQuadInNearPlane()
-        {
-            Vector3[] frustumCorners = new Vector3[4];
-            _camera.CalculateFrustumCorners(new Rect(0,0,1,1),0.1f, Camera.MonoOrStereoscopicEye.Mono, frustumCorners);
-            Vector3[] sortedCorners = { frustumCorners[0], frustumCorners[3], frustumCorners[1], frustumCorners[2] };
+        {       
+            _camera.CalculateFrustumCorners(new Rect(0,0,1,1),0.1f, Camera.MonoOrStereoscopicEye.Mono, _frustumCornersQuad);
+            Vector3[] sortedCorners = { _frustumCornersQuad[0], _frustumCornersQuad[3], _frustumCornersQuad[1], _frustumCornersQuad[2] };
             _silhouetteQuad.mesh.vertices = sortedCorners;
             _silhouetteQuad.mesh.RecalculateBounds();
         }
        void Update()
         {
            
-            _silhouetteMat.SetVector(_propertyIdPosition,Vector3.zero);
-            FitQuadInNearPlane();
+            _silhouetteMat.SetVector(_propertyIdPosition,WorldToQuad(_silhouetteWorldCenter.position));
         }
         #endregion
     }
