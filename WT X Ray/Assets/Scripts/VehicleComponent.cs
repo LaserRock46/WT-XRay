a9a55c20a1d89fe2e140deba6e8cd39224d09cb1
LaserRock46 <bialyrobert11@gmail.com>
2021-09-04 17:17:11 +0200

    Poprawa prefabów i modeli

--- WT X Ray/Assets/Scripts/VehicleComponent.cs
+++ WT X Ray/Assets/Scripts/VehicleComponent.cs
@@ -7,28 +7,42 @@ namespace Project.Uncategorized
    [AddComponentMenu(nameof(Project) + "/" + nameof(Uncategorized) + "/VehicleComponent")]
     public class VehicleComponent : MonoBehaviour
     {
-         #region Temp
+        #region Temp
         //[Header("Temporary Things", order = 0)]
         #endregion
 
         #region Fields
-        //[Header("Fields", order = 1)]
+        [Header("Fields", order = 1)]
+        [SerializeField] private int _maxDurability;
+        [SerializeField] private int _currentDurability;
+
+        [SerializeField] private Material[] _xRayMat;
+        [SerializeField] private LayerMask _layerXRay;
+        [SerializeField] private LayerMask _layerXRayRedOutline;
+        [SerializeField] private LayerMask _layerXRayRedAnimated;
+
+        [SerializeField] private MeshRenderer[] _componentRenderers;
         #endregion
 
         #region Functions
-        
+
         #endregion
 
-        
+
 
         #region Methods
-        void Start()
+        public void ResetComponent()
         {
-            
+            _currentDurability = _maxDurability;
+
         }
-       void Update()
+        void UpdateVisuals()
         {
             
+            foreach (MeshRenderer meshRenderer in _componentRenderers)
+            {
+                meshRenderer.material = _xRayMat[0];
+            }           
         }
         #endregion
     }
